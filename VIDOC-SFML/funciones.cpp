f8b7964ee062c36d1db5dc29e677973bc864a2e5
gaspaz <gaston.paz@alumnos.frgp.utn.edu.ar>
2020-10-20 17:31:38 -0300

    Proyecto
    nuevo

---
+++ VIDOC-SFML/funciones.cpp
@@ -0,0 +1,6 @@
+#include <SFML/Graphics.hpp>
+#include "juego.h"
+using namespace sf;
+///using namespace std;
+
+
---
+++ VIDOC-SFML/main.cpp
@@ -0,0 +1,12 @@
+#include <SFML/Graphics.hpp>
+#include "juego.h"
+using namespace sf;
+
+
+int main(int argc, char * args[])
+{
+juego * vidoc;
+vidoc = new juego(1280,720,"VIDOC91");
+
+
+return 0;}
